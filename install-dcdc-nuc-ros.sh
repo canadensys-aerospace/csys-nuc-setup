1a8996b46663433654a87f67c8da15bd5ccd1889
Jason Thomas <jason.thomas@canadensys.com>
2018-06-20 16:58:00 -0400

    add installation of dcdc-nuc-list utility

--- install-dcdc-nuc-ros.sh
+++ install-dcdc-nuc-ros.sh
@@ -11,6 +11,12 @@ sudo apt-get -y install libusb-dev
 
 sudo cp dcdc-nuc.rules /etc/udev/rules.d
 
+cd ~
+git clone https://github.com/canadensys-aerospace/csys-dcdc-nuc-list
+cd csys-dcdc-nuc-list
+make
+sudo make install
+
 source ~/catkin_ws/devel/setup.bash
 
 cd ~/catkin_ws/src
